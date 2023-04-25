024b4ad72c02448437ac9009e6bb6ce0de4e5eee
Jheanster <jctoledo935@gmail.com>
2023-04-24 22:21:14 -0400

    u5817

--- User Interfaces/screen3.gd
+++ User Interfaces/screen3.gd
@@ -8,15 +8,13 @@ extends Control
 
 func _on_CoopButton_pressed():
 	Global.typeM = 0
-	var options = load("res://User Interfaces/ConnectionSetUp.tscn").instance()
-	get_tree().current_scene.add_child(options)
+	get_tree().change_scene("res://User Interfaces/ConnectionSetUp.tscn")
 
 func _on_CompButton_pressed():
 	Global.typeM = 1
-	var options = load("res://User Interfaces/ConnectionSetUp.tscn").instance()
-	get_tree().current_scene.add_child(options)
+	get_tree().change_scene("res://User Interfaces/ConnectionSetUp.tscn")
+	
 
 
 func _on_BackButton_pressed():
-	var options = load("res://User Interfaces/Screen2.tscn").instance()
-	get_tree().current_scene.add_child(options)
+	get_tree().change_scene("res://User Interfaces/screen2.tscn")
